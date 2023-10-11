6b66c00ec51b8108b4b9fc0ab2091955581efd8b
Ilias Kadyrkulov <ilias.kadyrkulov@gmail.com>
2023-10-11 14:22:44 +0600

    add: slider dots, dummy images

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -5,6 +5,9 @@ import styles from './Slider.module.scss'
 import swfa from '../../assets/swfa.jpg'
 import { BsPlayCircle } from 'react-icons/bs'
 import { GrBookmark } from 'react-icons/gr'
+import disneyIcon from '../../assets/icons8-disney-plus.svg'
+import hboIcon from '../../assets/icons8-hbo.svg'
+import netflixIcon from '../../assets/icons8-netflix.svg'
 
 const images = [
     {
@@ -29,42 +32,81 @@ const images = [
 
 export const Carousel = () => {
     let settings = {
+        arrows: false,
         dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1
     }
+    let sSettings = {
+        infinite: false,
+        swipe: false,
+        speed: 500,
+        slidesToShow: 7,
+        slidesToScroll: 1
+    }
+
     return (
-        <Slider {...settings} className={styles.Carousel}>
-            {images.map((s, index) => (
-                <div key={index} className={styles.Slide}>
-                    <div
-                        className={`bg-cover bg-no-repeat h-full`}
-                        style={{ backgroundImage: `url(${s.url})` }}
-                    >
-                        <div className={styles.Details}>
-                            <h3 className="text-4xl text-slate-200 font-bold">
-                                {s.title}
-                            </h3>
-                            <span>{`• ${s.details}`}</span>
-                            <p className="font-medium text-gray-300">
-                                {s.desc}
-                            </p>
-                            <div className={styles.Buttons}>
-                                <button className={styles.TrailerButton}>
-                                    <BsPlayCircle />
-                                    <span>Watch Trailer</span>
-                                </button>
-                                <button className={styles.WatchButton}>
-                                    <GrBookmark />
-                                    <span>Add to Watchlist</span>
-                                </button>
+        <>
+            <Slider {...settings} className={styles.Carousel}>
+                {images.map((s, index) => (
+                    <div key={index} className={styles.Slide}>
+                        <div
+                            className={`bg-cover bg-no-repeat bg-center h-full`}
+                            style={{ backgroundImage: `url(${s.url})` }}
+                        >
+                            <div className={styles.Details}>
+                                <h3 className="text-4xl text-slate-200 font-bold">
+                                    {s.title}
+                                </h3>
+                                <span className="my-5 text-slate-400 opacity-95">{`• ${s.details}`}</span>
+                                <p className="font-medium text-gray-300">
+                                    {s.desc}
+                                </p>
+                                <div className={styles.Buttons}>
+                                    <button className={styles.TrailerButton}>
+                                        <BsPlayCircle />
+                                        <span>Watch Trailer</span>
+                                    </button>
+                                    <button className={styles.WatchButton}>
+                                        <GrBookmark />
+                                        <span>Add to Watchlist</span>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            ))}
-        </Slider>
+                ))}
+            </Slider>
+            <div className={styles.Inner}>
+                <Slider {...sSettings} className={styles.MiniCarousel}>
+                    <div className="flex">
+                        <img src={disneyIcon} />
+                    </div>
+                    <div>
+                        <img src={hboIcon} />
+                    </div>
+                    <div>
+                        <img src={netflixIcon} />
+                    </div>
+                    <div>
+                        <img src={disneyIcon} />
+                    </div>
+                    <div>
+                        <img src={hboIcon} />
+                    </div>
+                    <div>
+                        <img src={netflixIcon} />
+                    </div>
+                    <div>
+                        <img src={disneyIcon} />
+                    </div>
+                    <div>
+                        <img src={hboIcon} />
+                    </div>
+                </Slider>
+            </div>
+        </>
     )
 }
